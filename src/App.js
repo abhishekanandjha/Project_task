1e5cf317e456ebffd5a65029ba66f786792494e9
milangupta95 <milangupta95@gmail.com>
2023-05-23 17:25:41 +0530

    Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ForgotPassword from './Components/ForgotPassword';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Navbar from './Components/Navbar';
+import Corousel from './Components/Corousel';
+import LandingPage from './Components/LandingPage';
+import Footer from './Components/Footer';
+import { Route, Routes } from 'react-router';
+import Courses from './Components/Courses';
+import Contact from './Components/Contact';
+import CoursePage from './Components/CoursePage';
 function App() {
+  const img = [{url : '4.png'}, {url : '11.png'}, {url :'10.png'}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/home' element={<LandingPage />} />
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+        <Route path='/courses' element={<Courses />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/course' element={<CoursePage images={img} courseName='Aviation' desc='Enroll to our Aviation Course' />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
